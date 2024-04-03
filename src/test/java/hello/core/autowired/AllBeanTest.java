385ec9598145a8cd0cde3779de9a97e90ce8dab0
정소민 <yjk70702002@gmail.com>
2024-04-03 23:47:08 +0900

    Feat: 모든 빈 조회 구현 1

---
+++ src/test/java/hello/core/autowired/AllBeanTest.java
@@ -0,0 +1,26 @@
+package hello.core.autowired;
+
+import hello.core.discount.DiscountPolicy;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+import java.util.Map;
+
+public class AllBeanTest {
+    @Test
+    void findAllBean() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(DiscountService.class);
+    }
+    static class DiscountService {
+        private final Map<String, DiscountPolicy> policyMap;
+        private final List<DiscountPolicy> policies;
+
+        public DiscountService(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> policies) {
+            this.policyMap = policyMap;
+            this.policies = policies;
+            System.out.println("policyMap = " + policyMap);
+        }
+    }
+}
